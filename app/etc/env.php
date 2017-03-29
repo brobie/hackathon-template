ba4985c152b604c10e71fd81ab4babbd6baedcbf
Ben Robie <brobie@degdigital.com>
2017-03-29 15:46:26 -0500

    Initial Hackathon Template

---
+++ app/etc/env.php
@@ -0,0 +1,62 @@
+<?php
+return array (
+  'backend' => 
+  array (
+    'frontName' => 'adminpanel',
+  ),
+  'crypt' => 
+  array (
+    'key' => '4b52a980994fcdb7ab627ca0f27e26c6',
+  ),
+  'session' => 
+  array (
+    'save' => 'files',
+  ),
+  'db' => 
+  array (
+    'table_prefix' => '',
+    'connection' => 
+    array (
+      'default' => 
+      array (
+        'host' => 'localhost',
+        'dbname' => 'hackathon',
+        'username' => 'dev',
+        'password' => 'dev',
+        'model' => 'mysql4',
+        'engine' => 'innodb',
+        'initStatements' => 'SET NAMES utf8;',
+        'active' => '1',
+      ),
+    ),
+  ),
+  'resource' => 
+  array (
+    'default_setup' => 
+    array (
+      'connection' => 'default',
+    ),
+  ),
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'cache_types' => 
+  array (
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'full_page' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'translate' => 1,
+    'config_webservice' => 1,
+  ),
+  'install' => 
+  array (
+    'date' => 'Wed, 29 Mar 2017 20:25:00 +0000',
+  ),
+);
